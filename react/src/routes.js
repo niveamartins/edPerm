f8754de0437dd971514a346afe0ea3722838bb7b
Nivea <niveamartins@dcc.ufrj.br>
2020-04-14 19:13:30 -0300

    setando o projeto em React

---
+++ react/src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// npm install react-router-dom
+
+
+// Aqui iremos importar todas as páginas
+
+
+// Aqui definimos todas as rotas
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                
+            </Switch>
+        </BrowserRouter>
+    );
+}
