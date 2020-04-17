c6e2c84ea49b659b2615bc53961381f67fd78ce3
sidneyouteiro <sid@dcc.ufrj.br>
2020-04-16 21:49:08 -0300

    criaçao do relatorio_contato

--- relatorios/relatorio_frequencia.py
+++ relatorios/relatorio_frequencia.py
@@ -1,16 +1,19 @@
-#TODO: fazer uma classe que cujos atributos são os campos do json que será emitido
-#TODO: FALAR COM O MATHEUS SOBRE UM ALUNO SÓ PODER ESTAR EM UMA TURMA
-#TODO: 
 import datetime
 import json
+import aux_functions.registerDateTime as registerDateTime
+
+'''
+Frequência: frequência computada em dias, horas e minutos com base no horário de check-in do cursista. 
+Ele poderá acompanhar a própria carga-horária com base na tolerância informada pelo propositor da atividade.
+
+'''
 
 class RelatorioFrequencia():    
     def __init__(self, alunos,turma, cargaHoraria):
-        tempo = str(datetime.datetime.now()).split(' ')
-        self.alunos = alunos #array de alunos em uma determinada turma
+        self.alunos = alunos # array de alunos em uma determinada turma
         self.turma = turma # a turma dos candangos
         self.cargaHoraria = cargaHoraria # carga horaria da turma/materia
-        self.DataeHoraDeCriacao = tempo[0]+' '+tempo[1].split('.')[0]
+        self.DataeHoraDeCriacao = registerDateTime.registerDateTime()
 
     def FrequenciaPorAluno(self):
         ListaDasFrequencias = {}
@@ -20,6 +23,7 @@ class RelatorioFrequencia():
 
     def CriarJson(self):
         frequencia = self.FrequenciaPorAluno()
+        #TODO: checar a tolerancia de cada aluno de acordo com a tolerancia da turma e retornar um bool
         JSON = {
             "turma" : self.turma,
             "DataeHoraDeCriacao" : self.DataeHoraDeCriacao,
@@ -37,10 +41,10 @@ class Alunos():
 
 
 
-a = Alunos('Josnei',4)
-b = Alunos('Maria',3)
-c = Alunos('Pedro',7)
+#a = Alunos('Josnei',4)
+#b = Alunos('Maria',3)
+#c = Alunos('Pedro',7)
 
-Relatorio1 = RelatorioFrequencia([a,b,c], 'Calculo' , 60)
+#Relatorio1 = RelatorioFrequencia([a,b,c], 'Calculo' , 60)
 
-Relatorio1.CriarJson()
+#Relatorio1.CriarJson()
