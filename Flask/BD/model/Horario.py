9b1c2e85ea30638fe704759d4e78f3895c78fd3c
sidneyouteiro <sid@dcc.ufrj.br>
2020-04-27 16:14:44 -0300

    Models criados

---
+++ Flask/BD/model/Horario.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Integer, Time, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from BD.model.Base.Base import Base
+
+class Horario(Base):
+    __tablename__ = 'Horario'
+    idHorario = Column(Integer, primary_key=True)
+    HorarioIdTurma = Column(Integer, ForeignKey('Turma.idTurma'), nullable=False)
+    DiaDaSemana = Column(String(20), nullable=False)
+    HorarioInicio = Column(Time, nullable=False)
+    HorarioTermino = Column(Time, nullable=False)
---
+++ Flask/BD/model/Turma.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer,Numeric,Text, Enum, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from BD.model.Base.Base import Base
+
+class Turma(Base):
+    __tablename__ = 'Turma'
+    IdTurma = Column(Integer,primary_key=True)
+    IdResponsavel = Column(Integer, ForeignKey('User.Id'), nullable=False)
+    NomeCurso = Column(String(50), nullable=False)
+    CargaHorariaTotal = Column(Integer, nullable=False)
+    Tolerancia = Column(Integer, nullable=False)
+    Modalidade = Column(String, nullable=False)
+    TurmaTag = Column(String, nullable=False)
