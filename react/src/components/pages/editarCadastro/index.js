756535ca7f90d0490da6eda3d04544bee0ecfe22
Nivea <niveamartins@dcc.ufrj.br>
2020-05-07 11:55:50 -0300

    Página de Perfil, Edição e Dados

---
+++ react/src/components/pages/editarCadastro/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { NavBar } from '../../navbar'
+import { Footer } from '../../footer'
+import { Accessibility } from '../../accessibility'
+
+function EditarCadastro() {
+
+    //Integrar cadastro com js
+
+    // mudar quando for integrar:
+    // <input placeholder="Nome do Aluno" value={name} onChange={e => setName(e.target.value)}></input>
+    // onSubmit={handleCreate}
+    // <input placeholder="Código da Turma" value={turma} onChange={e => setTurma(e.target.value)}></input>
+
+    return (
+        <div>
+            <Accessibility/>
+            <NavBar/>
+
+            <main className="main-content-forms">
+                <div className="form-page-container">
+                    <div className="form-container">
+                        <form>
+                            <p>Edite suas informações abaixo</p>
+                            <input type="text" name="usuario" class="form-input" placeholder="Usuário" required />
+                            <input type="text" name="email" class="form-input" placeholder="Email" required></input>
+                            <input type="password" name="senha" class="form-input" placeholder="Senha" required />
+                            <input type="password" name="confirme_senha" id="confirm_password" class="form-input"
+                                placeholder="Confirme a Senha" required />
+                            <input type="submit" class="button login" value="Editar" />
+                    
+                        </form>
+                    </div>
+                </div>
+            </main>
+            <Footer/>
+        </div>
+    )
+}
+
+export default EditarCadastro
