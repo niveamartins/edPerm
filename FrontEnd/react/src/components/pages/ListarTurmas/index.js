3fb2f3ac3c52db77a39d315324070c422bdca3dd
KeithFabre <keithfmacedo@poli.ufrj.br>
2020-06-10 20:04:24 -0300

    ajustes no layout

--- FrontEnd/react/src/components/pages/ListarTurmas/index.js
+++ FrontEnd/react/src/components/pages/ListarTurmas/index.js
@@ -1,77 +1,78 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import AddIcon from '@material-ui/icons/Add';
-import { Link } from '../../../../node_modules/react-router-dom';
+import React, { Fragment, useState, useEffect } from "react"
+import AddIcon from "@material-ui/icons/Add"
+import { Link } from "../../../../node_modules/react-router-dom"
 
-import api from '../../../services/api'
-import { NavBar } from '../../navbar'
-import { Footer } from '../../footer'
-import { Accessibility } from '../../accessibility'
+import api from "../../../services/api"
+import { NavBar } from "../../navbar"
+import { Footer } from "../../footer"
+import { Accessibility } from "../../accessibility"
 
-import './listarTurmas.css'
-
+import "./listarTurmas.css"
 
 function ListarTurmas() {
+	const [turmas, setTurmas] = useState([])
 
-    const [turmas, setTurmas] = useState([]); 
+	useEffect(() => {
+		try {
+			api.get("listaturma").then((response) => {
+				setTurmas(response.data)
+			})
+		} catch (err) {
+			alert("Não foi possível encontrar as turmas, tente novamente")
+		}
+	}, [])
 
-    useEffect(() => {
-      try {
-        api
-        .get('listaturma')
-        .then(response => {
-            setTurmas(response.data);
-        });
-      } catch (err) {
-          alert("Não foi possível encontrar as turmas, tente novamente");
-      }  
-    }, [])
+	const getTurmasContent = (turma) => {
+		let content = []
+		for (let idx in turma) {
+			const item = turma[idx]
+			content.push(
+				<div className="card">
+					<table className="card-list">
+						<thead>
+							<tr className="title">
+								<td>{item.nome_do_curso}</td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr className="tutor">
+								<td>Responsável:</td>
+								<td>
+									<span className="tutor__highlight">
+										{item.nomeDoPropositor}
+									</span>
+								</td>
 
-    const getTurmasContent = turma => {
-        let content = [];
-        for (let idx in turma) {
-          const item = turma[idx];
-          content.push( 
-            <div className="card-container">
-                <div className="card">
-                    <table className="card-list">
-                        <thead>
-                            <tr className="title">
-                                <td>{item.nome_do_curso}</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className="tutor">
-                                <td>Responsável:</td>
-                                <td><span className="tutor__highlight">{item.nomeDoPropositor}</span></td>
-                                            
-                                <Link to= {{
-					            pathname: "/turma",
-					            state: [item.id_turma]
-					            }} className="link-info">
-                                    <AddIcon id="more-details"></AddIcon>info.
-                                </Link>
-                            </tr>
-                        </tbody>
+								<Link
+									to={{
+										pathname: "/turma",
+										state: [item.id_turma],
+									}}
+									className="link-info"
+								>
+									<AddIcon id="more-details"></AddIcon>info.
+								</Link>
+							</tr>
+						</tbody>
 
-                        <thead>
-                            <tr className="header">
-                                <th>Turma</th>
-                                <th>Informações</th>
-                            </tr>
-                        </thead>
+						<thead>
+							<tr className="header">
+								<th>Turma</th>
+								<th>Informações</th>
+							</tr>
+						</thead>
 
-                        <tbody>
-                            <tr className="content">
-                                <td className="name">Carga horária total</td>
-                                <td className="value">{item.Carga_Horaria_Total}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            )
+						<tbody>
+							<tr className="content">
+								<td className="name">Carga horária total</td>
+								<td className="value">{item.Carga_Horaria_Total}</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			)
 
-             /*<tr className="content">
+			/*<tr className="content">
                                 <td className="name">Tolerância</td>
                                 <td className="value">120h</td>
                             </tr>
@@ -87,25 +88,24 @@ function ListarTurmas() {
                                 <td className="name">Tag</td>
                                 <td className="value">Atividade Física</td>
                             </tr>*/
-          // Aqui dentro do push devemos colocar o html da parte de cada um dos contatos
-          }
-        
-        return content
-      };
+			// Aqui dentro do push devemos colocar o html da parte de cada um dos contatos
+		}
 
-    return (
-        <Fragment>
-            <Accessibility />
-            <NavBar />
-            <main className="main">
-                <div className="teste">
+		return content
+	}
+
+	return (
+		<Fragment>
+			<Accessibility />
+			<NavBar />
+			<main>
+				<div className="card-container">
                     {getTurmasContent(turmas)}
                 </div>
-            </main>
-            <Footer />
-        </Fragment>
-
-    )
+			</main>
+			<Footer />
+		</Fragment>
+	)
 }
 
 export default ListarTurmas
