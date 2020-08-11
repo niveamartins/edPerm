894bc5cddf48aea543c3eef3e43b564c430a9f1d
KeithFabre <keithfmacedo@poli.ufrj.br>
2020-08-11 12:16:10 -0300

    input tipo user

---
+++ FrontEnd/react/src/components/pages/CadastroUsuario/data/userData.js
@@ -0,0 +1,8 @@
+// user type
+export const userType = [
+   { label: "Administrador", value: "adm" },
+   { label: "Gestor", value: "gestor" },
+   { label: "Coordenador", value: "coordenador" },
+   { label: "Propositor", value: "propositor" },
+   { label: "Cursista", value: "cursista" },
+]
