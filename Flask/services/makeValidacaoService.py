57f1bc40f236a70d1c8a9d2f00ddb3271d4024ff
Sidney Outeiro <sid@dcc.ufrj.br>
2020-08-11 10:58:23 -0300

    aaaaaaaaaaaaaaaaaaaa

--- Flask/services/makeValidacaoService.py
+++ Flask/services/makeValidacaoService.py
@@ -6,15 +6,14 @@ class makeValidacaoService:
     def execute(self, dados):
         session = get_session()
         presencas = session.query(Presenca,PresencaTot).filter(Presenca.presencaValidade==0,Presenca.presenca_id_turma==dados['idTurma'],PresencaTot.presencatot_id_turma==dados['idTurma']).all()
-        horarioTermino = session.query(Turma.)
         if not presencas:
             return {'Error':'Não há presencas no banco de dados'}
 
         for presenca in presencas[0]:
-            if presenca is in dados['presencas']:
+            if True:
                 presenca.presencaValidade = 1
                 delta = presenca.CheckIn.time()- dados['HorarioDeTermino']
-                for presencatot in presencas[1]:
+                #for presencatot in presencas[1]:
 
             else:
                 session.delete(presenca)
